57d06cd1a248dbbe84132b5bdb8fd12ad57e9400
jeondui <jeondui@gmail.com>
2021-07-13 18:41:11 +0900

    initial commit

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,3 +1,13 @@
+/**
+ * Counting sort (계수 정렬)
+ * 배열의 원소끼리 비교하는 경우가 없으므로 비교적 속도는 빠른 편이다.
+ * 정렬하고자 하는 수의 범위가 커질수록 효율이 떨어진다. (메모리 낭비)
+ * 1. 초기 배열을 순회하며 각각의 원소의 값을 index로 가지는 배열에 counting을 증가시킨다.
+ * 2. counting배열의 크기는 초기 배열의 원소값의 최대값 + 1으로 설정한다.
+ * 3. counting배열의 원소를 누적합으로 바꿔준다.
+ * 4. counting배열을 순회하며 누적합을 통해 어떤 index에 위치해야하는지 맵핑 시킨다.
+ * 시간 복잡도 O(N)
+ */
 public class CountingSort {
 
     public static void printArray(int[] arr) {
